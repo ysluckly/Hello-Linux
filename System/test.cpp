10ce89ad8ff41cbce2aab667dd6cb1ae992aec47
Mr-Hunter <Hunter@localhost.localdomain>
2018-10-28 19:21:41 -0700

    test for github

---
+++ System/test.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name:    test.cpp
+	> Author:       Hunter
+	> Mail:         hunter.520@qq.com
+	> Created Time: Sun 28 Oct 2018 07:20:41 PM PDT
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello Linux!"<<endl;
+    return 0;
+}
