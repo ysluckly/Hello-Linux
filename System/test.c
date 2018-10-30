2713847c9dd594f32787d70d727408360f4e140f
Mr-Hunter <1522138135@qq.com>
2018-10-30 13:23:59 -0700

    test for git

---
+++ System/test.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name:    test.c
+	> Author:       Hunter
+	> Mail:         hunter.520@qq.com
+	> Created Time: Tue 30 Oct 2018 01:23:12 PM PDT
+ ************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+    printf("Hello LInux");
+    return 0;
+}
