e345f863fdd353e910353f095f7dc536ca25de92
Mr-Hunter <1522138135@qq.com>
2019-01-19 20:04:54 -0800

    python 模拟ls命令

---
+++ python/Day5/test4.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#coding:utf-8
+
+'''
+实现ls命令
+'''
+
+import os,sys
+
+path = sys.argv[1]
+#os.walk遍历指定目录，然后返回根目录，包含目录，包含文件名
+for root,dirname,filenames in os.walk(path):
+    for filename in filenames:
+        print os.path.join(root,filename)
