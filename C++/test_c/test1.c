c1e2a46389df5f138eefaa692d22bcae34145d3a
Mr-Hunter <1522138135@qq.com>
2019-04-10 20:59:19 +0800

    tecent的一个小坑

---
+++ C++/test_c/test1.c
@@ -0,0 +1,17 @@
+/*************************************************************************
+    > File Name:    test1.c
+    > Author:       Hunter
+    > Mail:         hunter.520@qq.com 
+    > Created Time: Wed 10 Apr 2019 08:21:33 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(int argc, char* argv[])
+{
+    long long a = 1,b=2,c=3;
+    printf("%d %d %d\n",a,b,c);
+
+    return 0;
+}
+
