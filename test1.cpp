e2e1d85038ec4e9ad683621da85984042b7574b0
Mr-Hunter <1522138135@qq.com>
2019-05-22 20:15:07 -0700

    ping test

---
+++ test1.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name:    test1.cpp
+	> Author:       Hunter
+	> Mail:         hunter.520@qq.com
+	> Created Time: Wed 22 May 2019 08:14:17 PM PDT
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout <<  "ping test" << endl;
+    return 0;
+}
